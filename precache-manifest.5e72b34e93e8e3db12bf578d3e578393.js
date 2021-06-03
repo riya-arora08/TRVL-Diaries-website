739bdb5a5072b8f8e87f89fb157d2322821919d8
Riya Arora <riyaarora2468@gmail.com>
2021-06-04 04:06:39 +0530

    Updates

---
+++ precache-manifest.5e72b34e93e8e3db12bf578d3e578393.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bc4d5542fb44dcaf9bcbcf913118eeb0",
+    "url": "/TRVL-Diaries-website/index.html"
+  },
+  {
+    "revision": "1bedbc8e9a279e3f2a5e",
+    "url": "/TRVL-Diaries-website/static/css/main.b3209779.chunk.css"
+  },
+  {
+    "revision": "36128ff7f373e5beff7a",
+    "url": "/TRVL-Diaries-website/static/js/2.1442e098.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/TRVL-Diaries-website/static/js/2.1442e098.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1bedbc8e9a279e3f2a5e",
+    "url": "/TRVL-Diaries-website/static/js/main.acbc53aa.chunk.js"
+  },
+  {
+    "revision": "2df9b0fd1beb2e447c03",
+    "url": "/TRVL-Diaries-website/static/js/runtime-main.1cae64d6.js"
+  }
+]);
